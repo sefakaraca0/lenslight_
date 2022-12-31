b8d74f10cc0ee3f5805d2750c4f5624d4e750c2b
MusaTG <49027797+MusaTG@users.noreply.github.com>
2022-12-31 20:31:59 +0300

    Create authMiddleware.js
    Closed #2

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,25 @@
+import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
+
+const checkUser = async (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.locals.user = null;
+        next();
+      } else {
+        const user = await User.findById(decodedToken.userId);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+export { authenticateToken, checkUser };
